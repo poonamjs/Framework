1e1fc62f1cf4c47052026fd347bc5425b6b74e1f
poonamjs <poonamjadhav54@gmail.com>
2022-05-14 13:27:16 +0530

    Second Commit

--- src/Demo/Addition.java
+++ src/Demo/Addition.java
@@ -3,8 +3,8 @@ package Demo;
 public class Addition {
 
 	public static void main(String[] args) {
-		int a=10;
-		int b=20;
+		int a=10, b=20;
+	
 		int sum=a+b;
 		
 		System.out.println("Addition ="+sum);
